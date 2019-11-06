d15201f8fc4e4b0a9bce1045cb14807df50f9b30
tatianimeneghini <tatiani.meneghini@gmail.com>
2019-11-05 22:11:56 -0300

    Adiciona o body-parser como dependência

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,18 +1,17 @@
-const contatos = require("../model/contatos");
+const model = require("../model/contatos");
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.model);
+  response.status(200).send(model.agenda);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
+const add = (request, response) => {
+ model.agenda.contatos.push(request.body)
+  response.status(200).send()
 };
-
 
 module.exports = {
   getAll,
-  getById
+  add
 }
 
