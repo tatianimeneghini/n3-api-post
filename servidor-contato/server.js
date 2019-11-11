438e89e686908405e34bc5e89f87511dde8a6da7
tatianimeneghini <tatiani.meneghini@gmail.com>
2019-11-11 19:24:32 -0300

    second commit

--- servidor-contato/server.js
+++ servidor-contato/server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app")
-const port = 3000
+const port = 5005
 
 app.listen(port, function () {
   console.log(`app está rodando na porta ${port}`)
--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -6,8 +6,20 @@ const getAll = (request, response) => {
 };
 
 const add = (request, response) => {
- model.agenda.contatos.push(request.body)
+  let info = request.body
+  info.id = Math.random().toString(36).substr(-8)
+  //Adiciona um id em uma fórmula randômica através do método post.
+  model.agenda.contatos.push(info)
   response.status(200).send()
+};
+
+function validarDataNascimento() {
+  let arrayDataNascimento = request.dataNascimento()
+  arrayDataNascimento.forEach(dataNascimento => {
+    if () {
+
+    }
+  })
 };
 
 module.exports = {
